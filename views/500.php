701051f6ece1308af3905bb8c8615754596d9764
John Robeson <johnny@localmomentum.net>
2010-04-17 01:43:31 -0400

    add more data to the 500 error template
    this should ultimately be be based on a development environment setting, but fitzgerald doesn't have that yet

--- views/500.php
+++ views/500.php
@@ -1,1 +1,9 @@
 <p class="error">Sorry, an error occurred.</p>
+<br />
+<strong>Number:</strong> <?php echo $number; ?>
+<br />
+<strong>Message:</strong> <?php echo $message; ?>
+<br />
+<strong>File:</strong> <?php echo $file; ?>
+<br />
+<strong>Line:</strong> <?php echo $line; ?>
