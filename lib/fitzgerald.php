e00ee35292bcec4619e06b98c842f8113b819870
John Robeson <johnny@localmomentum.net>
2010-06-23 15:55:03 -0400

    add halt($body, $statusCode = 200, $headers) method and small error($body,$statusCode = 400) wrapper
    this method outputs a body with an optional status code and http headers array

--- lib/fitzgerald.php
+++ lib/fitzgerald.php
@@ -211,13 +211,13 @@
             echo $this->render('404');
             exit(0);
         }
-        
+
         public function error($body, $statusCode = 400) {
             $this->halt($body, $statusCode);
         }
 
         public function halt($body = '', $statusCode = 200, $headers = array()) {
-        	   $this->setResponseCode($statusCode);
+            $this->setResponseCode($statusCode);
             $this->sendHeaders($headers);
             echo $body;
             exit(0);
